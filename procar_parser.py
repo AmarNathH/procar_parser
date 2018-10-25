cd93d5d1f885e849fecf1927b16bbcacfcff5268
AmarNathH <amarnath.h.96@gmail.com>
2018-10-25 15:40:10 +0530

    Added code to generate output files

--- procar_parser.py
+++ procar_parser.py
@@ -1,10 +1,16 @@
 import re
 import sys
+import os
+import shutil
 from numpy import empty
 
 if len(sys.argv)<2:
     print('Less number of arguments. Please pass the POSCAR file !')
     sys.exit()
+
+if os.path.exists("Output"):
+    print('Deleting old Output files')
+    shutil.rmtree("Output")
 
 procar = open(sys.argv[1])
 lines = procar.readlines()
@@ -30,13 +36,22 @@ kpoint_data_space = n_bands*band_data_space + 3
 i_k = 4-1   #line number from where the k-point table starts
 i_b = 2     #line number from where the band table starts
 
-#array to store poscar data
+#arrays to store poscar data
 poscar_data = [[[ 0 for i in range(n_ions)] for j in range(n_bands)] for k in range(n_kpoints)] 
 
+#generate files to store data
+for c in range(n_ions):
+    os.makedirs("Output/ion"+str(c+1))
+
+#write data
 for a in range(n_kpoints):
-    for b in range(n_bands):
-        for c in range(n_ions):
+    for c in range(n_ions):
+        for b in range(n_bands):
             poscar_data[a][b][c] = lines[i_k + a*kpoint_data_space + i_b + b*band_data_space + 3 + c]
+            files = open("Output/ion"+str(c+1)+"/band_{}".format(b+1),'a')
+            files.write(poscar_data[a][b][c])
+            files.close()
+
 
 
 
