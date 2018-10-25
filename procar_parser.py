16d25b31e92ec3b9cdcb65da1af9e39fc3a02582
AmarNathH <amarnath.h.96@gmail.com>
2018-10-25 08:42:43 +0530

    Added initial output

--- procar_parser.py
+++ procar_parser.py
@@ -1,4 +1,16 @@
 import re
-import pandas as pd
+
+procar = open('PROCAR')
+lines = procar.readlines()
+
+#Extract data regarding kpoints, nbands and nions
+n_kpoints = re.findall('\d+',lines[1])[0]
+n_bands = re.findall('\d+',lines[1])[1]
+n_ions = re.findall('\d+',lines[1])[2]
+
+print('Number of Kpoints:'+n_kpoints)
+print('Number of Bands:'+n_bands)
+print('Number of Ions:'+n_ions)
+
 
 
