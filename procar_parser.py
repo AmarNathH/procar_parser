5e9df19178cc8d9c172e4bbc0789648eb202525f
Amarnath <appu.only11@gmail.com>
2018-10-26 16:28:59 +0530

    Added code to generate band_combined file

--- procar_parser.py
+++ procar_parser.py
@@ -2,7 +2,6 @@ import re
 import sys
 import os
 import shutil
-from numpy import empty
 
 if len(sys.argv)<2:
     print('Less number of arguments. Please pass the POSCAR file !')
@@ -44,23 +43,38 @@ poscar_data = [[[ 0 for i in range(n_ions)] for j in range(n_bands)] for k in ra
 for c in range(n_ions):
     os.makedirs("Output/ion"+str(c+1))
 
-flag = 1
-
 #write data
-for a in range(n_kpoints):
-    for c in range(n_ions):
-        for b in range(n_bands):
-            poscar_data[a][b][c] = lines[i_k + a*kpoint_data_space + i_b + b*band_data_space + 3 + c]
+for c in range(n_ions):
+    for b in range(n_bands):
+        flag = 1
+        for a in range(n_kpoints):
+            poscar_data[a][b][c] = str(a+1) + "\t" + lines[i_k + a*kpoint_data_space + i_b + b*band_data_space + 3 + c]
             files = open("Output/ion"+str(c+1)+"/band_{}".format(b+1),'a')
 
             if flag:
-                files.write(lines[i_t])
+                files.write("kpoint\t" + lines[i_t])
+                flag = 0
+
+            files.write(poscar_data[a][b][c])
+            files.close()    
+
+
+#Loop to generate band_combined file for each ions
+for c in range(n_ions):
+    flag = 1
+    for b in range(n_bands):
+
+        for a in range(n_kpoints):
+            poscar_data[a][b][c] = str(a+1)+ "\t"+ lines[i_k + a*kpoint_data_space + i_b + b*band_data_space + 3 + c]
+            files = open("Output/ion"+str(c+1)+"/band_combined",'a')
+
+            if flag:
+                files.write("kpoint\t" + lines[i_t])
 
             files.write(poscar_data[a][b][c])
             files.close()
-    flag = 0    
-
-
+            flag = 0
+    
 
 
 
